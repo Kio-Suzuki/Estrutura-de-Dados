92ba21b1f7bb00d6864079d1c4470d30c9edbdbb
Akio Suzuki <116661015+Kio-Suzuki@users.noreply.github.com>
2023-08-21 22:34:53 -0300

    a03-01 create

---
+++ a03-01.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    int n = 1;
+    int a;
+    int b;
+    scanf("%d", &n); 
+    do{
+        n++;
+        a = 100 * sqrt(n);
+        b = pow(2, n);
+    }while(a > b);
+    printf("n = ", n);
+}
