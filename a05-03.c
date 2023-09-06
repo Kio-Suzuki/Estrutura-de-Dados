e52d40e0add308e23f1a84dc9a9dac5be93eeb9a
Akio Suzuki <kiosuzuki1@gmail.com>
2023-09-06 16:51:43 -0300

    a5-finalizado

--- a05-03.c
+++ a05-03.c
@@ -3,19 +3,21 @@
 
 int main()
 {
-    int matriz[3][3] ={{2,3,2},{5,6,9},{1,0,7}}, mx = 0, contA = 1, contB = 1;
+    int matriz[3][3] ={{2,3,2},{5,6,9},{1,0,7}}, Mx, contA = 1, contB = 1;
+    Mx = matriz[1][1];
     for(int i = 0; i < 3; i++){
         for(int j = 0; j < 3; j++){
             contA++;
-            if(matriz[i][j] > mx){
+            if(Mx < matriz[i][j]){
                 contB++;
-                mx = matriz[i][j];
+                Mx = matriz[i][j];
             }
         }
     }
 
-    printf("O maior valor da matriz = %d\n", mx);
+    printf("O maior valor da matriz = %d\n", Mx);
     printf("A operacao fundamental A foi executada %d vezes\n", contA);
     printf("A operacao fundamental B foi executada %d vezes", contB);
+
     return 0;
 }
